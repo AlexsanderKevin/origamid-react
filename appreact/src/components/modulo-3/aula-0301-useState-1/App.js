2d0de50c82615d0fe1e00b43f972498b861fadad
Kevi <kevin13348@gmail.com>
2022-12-21 11:53:18 -0300

    aula-0301-useState-2
    - callbacks

--- appreact/src/components/modulo-3/aula-0301-useState-1/App.js
+++ appreact/src/components/modulo-3/aula-0301-useState-1/App.js
@@ -2,19 +2,16 @@ import React from "react";
 
 const App = () => {
     const [ ativo, setAtivo ] = React.useState(true)
-    const [ dados, setDados ] = React.useState({ nome: 'Kevin', idade: 20 })
 
     function handleClick() {
-        setAtivo(!ativo);
-        setDados({...dados, faculdade: 'Sim'})
+        setAtivo( anterior => !anterior );
     }
 
     return (
         <>
-        <p>{dados.nome}</p>
-        <p>{dados.idade}</p>
-        <p>{dados.faculdade}</p>
-        <button onClick={handleClick} disabled={!ativo}>{ativo? "Ativo": "Inativo"}</button>
+        <button onClick={handleClick} disabled={!ativo}>
+            {ativo? "Ativo": "Inativo"}
+        </button>
         </>
     );
 };
