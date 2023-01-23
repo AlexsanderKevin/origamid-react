cb74c92334bce17a5c95c5d30d083afa45ba7b62
Kevi <kevin13348@gmail.com>
2023-01-23 09:38:06 -0300

    aula-0306-custom-hooks
    - useFetch

--- appreact/src/components/modulo-3/aula-0306/App.js
+++ appreact/src/components/modulo-3/aula-0306/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import Produto from './Produto'
+import useFetch from './useFetch'
 
 const App = () => {
-  return (
-    <Produto/>
-  )
+  const { data, loading, error, request } = useFetch()
+
+  React.useEffect(() => {
+    request( 'https://ranekapi.origamid.dev/json/api/produto/notebook' )
+  }, [ request ])
+
+  if ( error ) return <p>Erro</p> 
+  if ( loading ) return <p>Carregando...</p>
+  if ( data ) return <div>{ data.nome }</div>
+  else return null
 }
 
 export default App
