46e7a891f0e8b0fe790d1f6862382faed88d9cab
Kevi <kevin13348@gmail.com>
2023-02-08 08:33:59 -0300

    aula-0501

---
+++ appreact/src/components/modulo-5/aula-0501/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Title from './Title'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Title />
+      <p className='text'>Meu App</p>
+    </div>
+  )
+}
+
+export default App
