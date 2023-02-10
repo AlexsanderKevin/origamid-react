47470991ea956713c0795e241b3f846ac5df0d45
Kevi <kevin13348@gmail.com>
2023-02-10 14:54:34 -0300

    aula-0602 Link

---
+++ appreact/src/components/modulo-6/aula-0602/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Footer from './Footer'
+import Header from './Header'
+import Home from './Home'
+import Login from './Login'
+import NotFound from './NotFound'
+import Sobre from './Sobre'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='sobre' element={ <Sobre/> } />
+        <Route path='*' element={ <NotFound/> } />
+        <Route path='login' element={ <Login/> } />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default App
